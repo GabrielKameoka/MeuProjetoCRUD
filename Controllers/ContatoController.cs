8480c3646a5b7c27235386ec25637dda2b269a06
GabrielKameoka <gabrielkameoka@gmail.com>
2024-09-24 19:29:41 -0300

    Adicionando funcionalidades

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -2,11 +2,74 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MeuProjetoCRUD.Context;
+using MeuProjetoCRUD.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MeuProjetoCRUD.Controllers
 {
-    public class ContatoController
+    [ApiController]
+    [Route("[controller]")]
+    public class ContatoController : ControllerBase
     {
-        
+        private readonly UsuarioContext _context;
+
+        public ContatoController(UsuarioContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("CadastrarUsuário")]
+        public IActionResult Create(Contato contato)
+        {
+            _context.Add(contato);
+            _context.SaveChanges();
+            return Ok(contato);
+        }
+
+        [HttpGet("BuscaPorId")]
+        public IActionResult BuscaPorId(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+
+            if (contato == null)
+            {
+                return NotFound();
+            }
+            return Ok(contato);
+        }
+
+        [HttpPut("AtualizaPorId")]
+        public IActionResult Atualizar(int id, Contato contato)
+        {
+            var contatoBanco = _context.Contatos.Find(id);
+
+            if(contatoBanco == null)
+            {
+                return NotFound();
+            }
+            contatoBanco.Nome = contato.Nome;
+            contatoBanco.Email = contato.Email;
+
+            _context.Contatos.Update(contatoBanco);
+            _context.SaveChanges();
+
+            return Ok(contatoBanco);
+        }
+
+        [HttpDelete("DeletaPorId")]
+        public IActionResult Deletar(int id)
+        {
+            var contatoBanco = _context.Contatos.Find(id);
+
+            if (contatoBanco == null)
+            {
+                return NotFound();
+            }
+            _context.Contatos.Remove(contatoBanco);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
     }
 }
--- obj/Debug/net8.0/MeuProjetoCRUD.AssemblyInfo.cs
+++ obj/Debug/net8.0/MeuProjetoCRUD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeuProjetoCRUD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d0cf0c13581095f83cdc64947b8e34b515f6a45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd36aa0918a7ac46d0069537ebce7f690817bff8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeuProjetoCRUD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeuProjetoCRUD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
